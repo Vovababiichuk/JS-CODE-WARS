3c9f54022db53e68f8b8bd71e922e4f109415572
Volodymyr <babihuk888@gmail.com>
2024-07-03 09:23:23 +0300

    task-4 valid pin

--- 04-validate-pin/index.js
+++ 04-validate-pin/index.js
@@ -8,17 +8,33 @@ Examples (Input --> Output)
 "12345"  -->  false
 "a234"   -->  false
 */
-
 function validatePIN(pin) {
 	if (pin.length !== 4 && pin.length !== 6) {
 		return false
 	}
-	return [...pin].every(char => !isNaN(char))
+	return [...pin].every(char => char >= '0' && char <= '9')
 }
 
 console.log(validatePIN('1234'))
 console.log(validatePIN('12345'))
 console.log(validatePIN('a234'))
+
+console.log(validatePIN('.234'))
+
+console.log(validatePIN('2 34'))
+console.log(validatePIN('2345 6'))
 // "1234"   -->  true
 // "12345"  -->  false
 // "a234"   -->  false
+
+console.log(validatePIN("1"))
+console.log(validatePIN("12"))
+console.log(validatePIN("123"))
+console.log(validatePIN("1234567"))
+console.log(validatePIN("-1.234"))
+console.log(validatePIN("00000000"))
+
+//! =========Best Practice================
+// function validatePIN(pin) {
+//   return /^(\d{4}|\d{6})$/.test(pin)
+// }
