1181b85ff8eef9b23268143c352c5152a5ba44e0
Volodymyr <babihuk888@gmail.com>
2024-07-10 12:23:27 +0300

    task14

--- 14-Calculate-average/index.js
+++ 14-Calculate-average/index.js
@@ -1,3 +1,5 @@
+// v1
+
 function findAverage(array) {
 	if (array.length === 0) return 0;
 
@@ -7,3 +9,15 @@ function findAverage(array) {
 console.log(findAverage([1,2,3]));
 console.log(findAverage([1,2,3,4]));
 console.log(findAverage([]));
+
+// v2
+
+function findAverage(array) {
+	return array.length !== 0
+		? array.reduce((acc, curr) => acc + curr, 0) / array.length
+		: 0
+}
+
+console.log(findAverage([1, 2, 3]))
+console.log(findAverage([1, 2, 3, 4]))
+console.log(findAverage([]))
