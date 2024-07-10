e0ed04300246fb0ccc4995182989822ac130cb98
Volodymyr <babihuk888@gmail.com>
2024-07-10 10:39:51 +0300

    task11

---
+++ 11-Square(n)-Sum/index.js
@@ -0,0 +1,11 @@
+/*!SECTION
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1**2 + 2**2 + 2**2 = 9.
+
+Допишіть функцію квадратної суми так, щоб вона підносила до квадрату кожне передане їй число, а потім підсумовувала результати.
+*/
+
+function squareSum(numbers){
+
+}
