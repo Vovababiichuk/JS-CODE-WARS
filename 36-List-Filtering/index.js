a7dff2f1f2a7a1fbd2cca25291e22015075bf275
Volodymyr <babihuk888@gmail.com>
2024-07-17 14:35:29 +0300

    task36

---
+++ 36-List-Filtering/index.js
@@ -0,0 +1,15 @@
+/*!SECTION
+У цьому завданні ви створите функцію, яка отримує список невід'ємних цілих чисел та рядків і повертає новий список з відфільтрованими рядками.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
+
+function filter_list(l) {
+	return l.filter(char => typeof char === 'number');
+}
+
+console.log(filter_list([1,2,'a','b']));
+console.log(filter_list([1,'a','b',0,15]));
