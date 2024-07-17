9c04d08f6568782dc51c8331a0cc0ae766dea605
Volodymyr <babihuk888@gmail.com>
2024-07-17 11:23:23 +0300

    task31

--- 00-draft/index.js
+++ 00-draft/index.js
@@ -3,3 +3,8 @@
 
 //TODO - CREATE A NEW ARRAY with numbers from 1 to n
 console.log(new Array(n).fill(0).map((_, i) => i + 1))
+
+//TODO - 10% discount
+
+let result = 120 * 0.9;
+console.log(result); // 108
