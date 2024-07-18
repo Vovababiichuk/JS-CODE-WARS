76a8692b05cab1b69a5366f243da9b2e8d70c93c
Volodymyr <babihuk888@gmail.com>
2024-07-18 17:18:40 +0300

    task45

---
+++ 45-Opposite-number/index.js
@@ -0,0 +1,19 @@
+/*!SECTION
+
+Дуже просто, за заданим числом (цілим / десятковим / обома, залежно від мови) знайти його протилежність (адитивну інверсію).
+
+Приклади:
+
+1: -1
+14: -14
+-34: 34
+
+*/
+
+function opposite(number) {
+  return -number
+}
+
+console.log(opposite(1));
+console.log(opposite(14));
+console.log(opposite(-34));
