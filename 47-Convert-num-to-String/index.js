6b642c0183e67a106444e94c93e37a0e1543be43
Volodymyr <babihuk888@gmail.com>
2024-07-18 18:02:54 +0300

    task47

--- 47-Convert-num-to-String/index.js
+++ 47-Convert-num-to-String/index.js
@@ -12,5 +12,15 @@
 */
 
 function numberToString(num) {
-  // Return a string of the number here!
+	// return String(num)
+	// return num + ''
+	// return `${num}`
+	// return JSON.stringify(num)
+	// return num.toFixed()
+
+	return num.toString()
 }
+
+console.log(numberToString(123));
+console.log(numberToString(999));
+console.log(numberToString(-100));
