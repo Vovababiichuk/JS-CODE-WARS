5412080d957b89d81274f5fd31450b2c589105a4
Volodymyr <babihuk888@gmail.com>
2024-07-29 17:31:41 +0300

    task66

---
+++ 66-Valid-Spacing/index.js
@@ -0,0 +1,29 @@
+/*!SECTION
+Вам потрібно написати функцію з іменем valid_spacing() або validSpacing(), яка перевіряє, чи має рядок допустимі інтервали. Функція повинна повертати або true, або false (або відповідне значення у кожній мові).
+
+У цій каталозі допустимим інтервалом вважається один пробіл між словами, без початкових та кінцевих пробілів. Словами можуть бути будь-які послідовні послідовності символів без пробілів. Нижче наведено кілька прикладів того, що повинна повертати функція:
+
+* 'Hello world'   => true
+* ' Hello world'  => false
+* 'Hello world  ' => false
+* 'Hello  world'  => false
+* 'Hello'         => true
+
+Even though there are no spaces, it is still valid because none are needed:
+* 'Helloworld'    => true
+* 'Helloworld '   => false
+* ' '             => false
+* ''              => true
+
+Зауважте - у вхідному рядку не буде жодних розділових знаків чи цифр, лише літери.
+
+*/
+
+// v1
+function validSpacing(s) {
+	return s.split(' ').every(word => word.trim().length > 0);
+}
+console.log(validSpacing('Hello world'));
+console.log(validSpacing(' Hello world'));
+console.log(validSpacing('Helloworld'));
+console.log(validSpacing('Helloworld '));
